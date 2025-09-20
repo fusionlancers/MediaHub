45423e3fd35749aaa677421a074cf41b18c121eb
Revanth Vetcha <vetcharevanth4@gmail.com>
2025-09-20 10:59:06 +0530

    Add Message model and fix all imports for full deployment

--- src/models/message.js
+++ src/models/message.js
@@ -1,14 +1,3 @@
-// // src/models/Message.js
-// const mongoose = require('mongoose');
-
-// const messageSchema = new mongoose.Schema({
-//   sender: { type: String, required: true },
-//   message: { type: String, required: true },
-//   createdAt: { type: Date, default: Date.now }
-// });
-
-// module.exports = mongoose.model('Message', messageSchema);
-
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
