0d22137b01eff60adad0876120d1089a600ef61d
Revanth Vetcha <vetcharevanth4@gmail.com>
2025-09-20 10:08:32 +0530

    Fix start script to node src/index.js for Render

--- src/models/media.js
+++ src/models/media.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+
 const mediaSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, default: '' },
-  fileId: { type: Schema.Types.ObjectId, required: true }, // GridFS file id
+  fileId: { type: Schema.Types.ObjectId, required: true }, 
   filename: { type: String },
   contentType: { type: String },
   uploadDate: { type: Date, default: Date.now },
-  userEmail: { type: String, required: true } // Add userEmail to associate media with user
+  userEmail: { type: String, required: true } 
 });
 
 module.exports = mongoose.model('Media', mediaSchema);
